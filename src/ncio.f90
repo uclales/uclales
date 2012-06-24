6ec1c321f7c76406f95608f1f016a480407f5c44
Linda Schlemmer <m300208@tornado1.dkrz.de>
2012-06-24 16:00:13 +0200

    write out theta_l instead of theta in 3D fields

--- src/ncio.f90
+++ src/ncio.f90
@@ -409,7 +409,7 @@ contains
     iret = nf90_put_var(ncid0, VarID, a_wp(:,i1:i2,j1:j2), start=ibeg,    &
          count=icnt)
     iret = nf90_inq_varid(ncid0, sanal(14), VarID)
-    iret = nf90_put_var(ncid0, VarID, a_theta(:,i1:i2,j1:j2), start=ibeg, &
+    iret = nf90_put_var(ncid0, VarID, a_tp(:,i1:i2,j1:j2)+th00, start=ibeg, &
          count=icnt)
     iret = nf90_inq_varid(ncid0, sanal(15), VarID)
     iret = nf90_put_var(ncid0, VarID, press(:,i1:i2,j1:j2), start=ibeg, &
@@ -557,7 +557,7 @@ contains
        if (itype==1) ncinfo = 'm/s'
        if (itype==2) ncinfo = 'mttt'
     case('t')
-       if (itype==0) ncinfo = 'Potential temperature'
+       if (itype==0) ncinfo = 'Liquid Water Potential temperature'
        if (itype==1) ncinfo = 'K'
        if (itype==2) ncinfo = 'tttt'
     case('p')
