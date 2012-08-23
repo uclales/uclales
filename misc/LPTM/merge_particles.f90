a0584799d14c996be1443fe21105686b8ec32f07
Bart van Stratum <bart@vanstratum.com>
2012-08-23 16:00:24 +0200

    Added time to merge_particles script

--- misc/LPTM/merge_particles.f90
+++ misc/LPTM/merge_particles.f90
@@ -63,10 +63,10 @@ program mergeparticles
   call check(nf90_create(path_out,nf90_clobber,ncid_out))
   ! Create dimensions
   call check(nf90_def_dim(ncid_out,'particles',nptot,pdim_out))
-  call check(nf90_def_dim(ncid_out,'time',nf90_unlimited,tdim_out))
+  call check(nf90_def_dim(ncid_out,'time',ntime,tdim_out))
   ! Create variables
-  call check(nf90_def_var(ncid_out,'particles',nf90_double,(/pdim_out/),pid_out))
-  call check(nf90_def_var(ncid_out,'time',nf90_double,(/tdim_out/),pid_out))
+  !call check(nf90_def_var(ncid_out,'particles',nf90_double,(/pdim_out/),pid_out))
+  call check(nf90_def_var(ncid_out,'time',nf90_double,(/tdim_out/),tid_out))
   call check(nf90_def_var(ncid_out,trim(var),nf90_float,(/pdim_out,tdim_out/),varid_out))
   ! Leave define mode
   call check(nf90_enddef(ncid_out))
@@ -95,6 +95,17 @@ program mergeparticles
   
       loc = loc + npin
       deallocate(var_read)
+
+      print*,i,j
+
+      if(i .eq. nx .and. j .eq. ny) then
+        allocate(var_read(nptot))
+        call check(nf90_inq_varid(ncid_in, 'time', varid_in))
+        call check(nf90_get_var(ncid_in,  varid_in, var_read, start=(/1/), count=(/ntime/)))
+        call check(nf90_put_var(ncid_out, tid_out, var_read, start=(/1/), count=(/ntime/))) 
+        deallocate(var_read)
+      end if
+
       call check(nf90_close(ncid_in))
 
       print*,'Processed ',trim(path),(i-1)*nx+j,'/',nx*ny 
