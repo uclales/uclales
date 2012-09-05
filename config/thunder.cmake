d8999d7af51c3e8faab705074591b32aaaf66e04
Ann Kristin Naumann <m300218@mpipc122.mpi.zmaw.de>
2012-09-05 09:05:03 +0200

    added config/thunder.cmake

---
+++ config/thunder.cmake
@@ -0,0 +1,5 @@
+set (ENV{NETCDF_ROOT} /sw/squeeze-x64/netcdf-latest-static-intel12)
+set (ENV{HDF5_ROOT} /sw/squeeze-x64/hdf5-latest-static)
+set (ENV{FFTW_ROOT} /sw/squeeze-x64/fftw-3.2.1)
+set (NETCDF_USE_STATIC_LIBRARIES TRUE)
+set (HDF5_USE_STATIC_LIBRARIES TRUE)
