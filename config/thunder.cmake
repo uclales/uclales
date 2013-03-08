3b9cbafd7d867c092c0b46fc53236c957e04eee5
Thijs Heus <m300063@thunder4.zmaw.de>
2013-03-08 13:22:18 +0100

    Clean up of the config/cmake

--- config/thunder.cmake
+++ config/thunder.cmake
@@ -1,6 +1,4 @@
-set (ENV{NETCDF_ROOT} ENV{HOME})
-set (HDF5_ROOT_DIR_HINT ENV{HOME})
-set (ENV{FFTW_ROOT} ENV{HOME})
-set (CMAKE_LIBRARY_PATH /usr/lib64)
+set (ENV{NETCDF_ROOT} /sw/squeeze-x64/netcdf-4.2-static/:/sw/squeeze-x64/netcdf_fortran-latest-static-intel13)
+set (ENV{HDF5_ROOT} /sw/squeeze-x64/hdf5-1.8.8-static)
 set (NETCDF_USE_STATIC_LIBRARIES TRUE)
 set (HDF5_USE_STATIC_LIBRARIES TRUE)
