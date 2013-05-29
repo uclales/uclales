d43c373fc96148326a0b9d40339d23a1983341dd
Thijs Heus <thijs@localhost.localdomain>
2013-05-29 09:50:40 +0200

    New Cmake setup

--- config/thunder.cmake
+++ config/thunder.cmake
@@ -1,4 +1,15 @@
-set (ENV{NETCDF_ROOT} /sw/squeeze-x64/netcdf-4.2-static/:/sw/squeeze-x64/netcdf_fortran-latest-static-intel13)
-set (ENV{HDF5_ROOT} /sw/squeeze-x64/hdf5-1.8.8-static)
-set (NETCDF_USE_STATIC_LIBRARIES TRUE)
-set (HDF5_USE_STATIC_LIBRARIES TRUE)
+# Thunder
+set(CMAKE_Fortran_COMPILER "ifort")
+set(Fortran_COMPILER_WRAPPER mpif90)
+
+set(USER_Fortran_FLAGS "-traceback -r8 -ftz -extend_source")
+set(USER_Fortran_FLAGS_RELEASE "-O3 -no-prec-div -xAVX")
+set(USER_Fortran_FLAGS_DEBUG "-fpe0 -O0 -g -check all -check nopointers -check noarg_temp_created")
+
+set(NETCDF_INCLUDE_DIR "/sw/squeeze-x64/netcdf_fortran-latest-static-intel13/include")
+set(NETCDF_LIB_1       "/sw/squeeze-x64/netcdf_fortran-latest-static-intel13/lib/libnetcdff.a")
+set(NETCDF_LIB_2       "/sw/squeeze-x64/netcdf-4.2-static/lib/libnetcdf.a")
+set(HDF5_LIB_1         "/sw/squeeze-x64/hdf5-1.8.7-static/lib/libhdf5.a")
+set(HDF5_LIB_2         "/sw/squeeze-x64/hdf5-1.8.7-static/lib/libhdf5_hl.a")
+set(SZIP_LIB           "")
+set(LIBS ${FFTW_LIB} ${NETCDF_LIB} ${HDF5_LIB} ${SZIP_LIB} m z curl)
