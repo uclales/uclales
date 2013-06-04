abadf2eb39ef43f47cfd73b9d90ee70a5461bbbc
Thijs Heus <m300063@blizzard1.(none)>
2013-06-04 16:54:31 +0200

    Blizzard.cmake did not compile. Now it does

--- config/blizzard.cmake
+++ config/blizzard.cmake
@@ -1,6 +1,6 @@
 # ARCH Linux
 set(CMAKE_Fortran_COMPILER "xlf")
-set(Fortran_COMPILER_WRAPPER mpfort)
+set(Fortran_COMPILER_WRAPPER mpxlf90)
 
 set(USER_Fortran_FLAGS "-qfree=F90 -qrealsize=8  -qwarn64 -qnosave -qinitauto=FFF00000 -qflttrap=en:ov:zero:inv:imp -qflag=w:e")
 set(USER_Fortran_FLAGS_RELEASE "-O4 -qnoipa -qstrict=none:exceptions -qinitauto=ff -qsigtrap")
@@ -12,4 +12,4 @@ set(NETCDF_LIB_2       "/sw/aix61/netcdf-4.1.2-hdf5-threadsafe/lib/libnetcdf.a")
 set(HDF5_LIB_1         "/sw/aix61/hdf5-1.8.6-threadsafe/lib/libhdf5_hl.a")
 set(HDF5_LIB_2         "/sw/aix61/hdf5-1.8.6-threadsafe/lib/libhdf5.a")
 set(SZIP_LIB           "")
-set(LIBS ${NETCDF_LIB_1} ${NETCDF_LIB_2} ${HDF5_LIB_1} ${HDF5_LIB_2} ${SZIP_LIB} m z curl)
+set(LIBS ${NETCDF_LIB_1} ${NETCDF_LIB_2} ${HDF5_LIB_1} ${HDF5_LIB_2} ${SZIP_LIB} m z)
